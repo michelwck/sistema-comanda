825ed44de18737816bfc8c69426a84ee1d4c2c9f
unknown <michel.wck@gmail.com>
2026-01-24 14:19:37 -0300

    fix issues

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
     server: {
+        allowedHosts: true,
         proxy: {
             '/api': {
                 target: 'http://localhost:3000',
